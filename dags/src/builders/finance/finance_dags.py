11d4e9b24388ed47c1767f3758d4aee6f68010e4
Maria Rivas O'Connor <160785139+mrivastn@users.noreply.github.com>
2025-02-28 17:14:58 -0300

    Deploy circleci (#20)
    * Empty commit for testing purposes
    
    * circleci init
    
    * test deploy
    
    * test deploy nubeproduct
    
    * test deploy nubeproduct 2
    
    * fix config
    
    * fix config
    
    * added circleci config for dags/src
    
    * test dags deployment onto s3
    
    * test deploy nubeproduct 3
    
    * fix error on parameters circleci
    
    * fix error on s3 bucket sync test
    
    * fix error on s3 bucket sync test 2
    
    * fix error on s3 bucket sync test 2
    
    * fix error on s3 bucket sync test 2
    
    * fix error on s3 bucket sync test 2

--- dags/src/builders/finance/finance_dags.py
+++ dags/src/builders/finance/finance_dags.py
@@ -15,8 +15,7 @@ default_args = {
     'on_failure_callback': task_fail_slack_alert_bi
 }
 
-
-# Crear el DAG frecuencia diaraia
+# Crear el DAG frecuencia diaria
 dag = create_dbt_dag(
     dag_id='dbt_finance_daily',
     schedule_interval_tag='daily-morning',
