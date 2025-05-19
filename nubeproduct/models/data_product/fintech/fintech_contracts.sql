0f259bc465627c676ff889f1d7b9d4fdfc0b6590
sofiasanchez <sofia.sanchez@tiendanube.com>
2025-05-19 13:58:00 -0300

    add fintech dags

--- nubeproduct/models/data_product/fintech/fintech_contracts.sql
+++ nubeproduct/models/data_product/fintech/fintech_contracts.sql
@@ -3,7 +3,7 @@
         materialized='table',
         unique_key='id',
         on_schema_change='fail',
-        tags=["fintech", "daily-9am", "daily-4pm"]
+        tags=["daily-9am", "daily-4pm"]
     )
 }}
 
