33c87c9ea95f38a811c921eb793a65fe9fab3370
usuario <usuario@ip-192-168-0-115.ec2.internal>
2025-05-19 17:41:50 -0300

    data product + first visit

--- nubeproduct/models/intermediate/marketing/_int_marketing__ga4_first_visit.sql
+++ nubeproduct/models/intermediate/marketing/_int_marketing__ga4_first_visit.sql
@@ -1,6 +1,6 @@
 SELECT
     user_pseudo_id,
-    first_visit_date
-FROM "refined"."ga4"."first_visit"
+    min(first_visit_date) as first_visit_date
+FROM {{ source('int_ga4', 'first_visit') }}
 GROUP BY user_pseudo_id
 
