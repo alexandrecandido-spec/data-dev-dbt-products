a6f4399437c983c099d6368a023652e46fa4cf6d
rodrint <140170172+rodrint@users.noreply.github.com>
2025-05-26 16:31:47 -0300

    Finance rnt/freqs updated (#74)
    * freqs updated
    
    * update

--- nubeproduct/models/data_product/finance/finance_exchange_rate.sql
+++ nubeproduct/models/data_product/finance/finance_exchange_rate.sql
@@ -4,7 +4,7 @@
         unique_key=['isocode','processed_at'],
         incremental_strategy='merge',
         on_schema_change='fail',
-        tags=["daily-6am-6pm"]
+        tags=["daily-6am","daily-6pm"]
     )
 }}
 
