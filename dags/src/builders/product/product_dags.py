8b9b5e5243e26fc70e2d3feb6cf29f9d02b1efb5
fabricriv <fabrizio.crivella@tiendanube.com>
2025-05-26 17:48:08 +0000

    Se generan los DAGs de product

---
+++ dags/src/builders/product/product_dags.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from airflow.models import Variable
+import ast
+from datetime import datetime
+from src.core.run_dbt_custom import create_dbt_dag
+from src.core.utils.slack_manager import task_fail_slack_alert_bi
+from functools import partial
+
+SLACK_IDS= ast.literal_eval(Variable.get('slack_ids_analytics_engineer_alert'))
+
+
+
+default_args = {
+    'owner': 'Fabrizio Crivella',
+    'depends_on_past': False,
+    'start_date': datetime(2025, 5, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'on_failure_callback': partial(task_fail_slack_alert_bi,slack_ids=SLACK_IDS)
+
+}
+
+# Crear el DAG frecuencia diaria
+dag = create_dbt_dag(
+    dag_id='dbt_product_daily',
+    schedule_interval_tag='daily-4am',
+    default_args=default_args,
+    tags=['product','daily-6am']
+)
+
