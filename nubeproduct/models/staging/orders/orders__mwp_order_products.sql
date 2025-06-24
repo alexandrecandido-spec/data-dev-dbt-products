27fb30e5b5f56c80b1d864f9b90494521456697f
rodrint <140170172+rodrint@users.noreply.github.com>
2025-06-24 09:10:16 -0300

    incremental fix (#116)

--- nubeproduct/models/staging/orders/orders__mwp_order_products.sql
+++ nubeproduct/models/staging/orders/orders__mwp_order_products.sql
@@ -25,7 +25,7 @@ with source as (
     {% endif %}
 ),
 existing_data AS (
-    {{ get_existing_data(this, ['order_id', 'product_id', 'sys_audit_created_on', 'sys_audit_created_by']) }}
+    {{ get_existing_data(this, ['id', 'sys_audit_created_on', 'sys_audit_created_by']) }}
 )
 
 
@@ -44,4 +44,4 @@ select
     current_timestamp AS sys_audit_updated_on,
     'data-dev-dbt-products' AS sys_audit_updated_by
 from source
-LEFT JOIN existing_data e ON source.order_id = e.order_id AND source.product_id = e.product_id
+LEFT JOIN existing_data e ON source.id = e.id
