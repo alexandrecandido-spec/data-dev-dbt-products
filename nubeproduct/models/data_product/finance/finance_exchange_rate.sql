ca7390d10df08c499192956ceb44739abb1ac5be
rodrint <140170172+rodrint@users.noreply.github.com>
2025-07-01 15:29:00 -0300

    Update finance_exchange_rate.sql (#121)

--- nubeproduct/models/data_product/finance/finance_exchange_rate.sql
+++ nubeproduct/models/data_product/finance/finance_exchange_rate.sql
@@ -26,6 +26,6 @@ WHERE
         source.processed_at >= '2018-01-01'
     {% endif %}
     {% if is_incremental() %}
-        source.processed_at > (SELECT MAX(processed_at) AS max_processed_at
-                            FROM {{ source('dp_finance', 'finance_exchange_rate') }})
+        source.processed_at >= (SELECT MAX(processed_at) AS max_processed_at
+                            FROM {{ this }})
     {% endif %}
