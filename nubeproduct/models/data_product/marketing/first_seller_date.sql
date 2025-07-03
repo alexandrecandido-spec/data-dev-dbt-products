6e430bb5ce006bccb4f04208de32f2333e0d7a8d
Giselle Galli <giselle.galli@tiendanube.com>
2025-07-03 15:12:31 -0300

    create first seller

--- nubeproduct/models/data_product/marketing/first_seller_date.sql
+++ nubeproduct/models/data_product/marketing/first_seller_date.sql
@@ -3,28 +3,34 @@
         materialized='incremental',
         incremental_strategy='merge',
         unique_key=['store_id'],
+        partition_by = 'year_month_day_code',
         on_schema_change='fail',
         tags=["marketing","daily-9am"]
     )
 }}
 
 WITH marketing AS (
-    SELECT *
+    SELECT store_id, 
+            year_month_day_code
     FROM {{ ref('marketing_attribution_model') }}
-    WHERE year_month_day_code >= 20200101
+    WHERE year_month_day_code >= 20230101
 ),
+
 qualified_orders as (
     SELECT *
     FROM {{ ref('_int__first_seller_7_or_more_sales_90d') }}
 ),
+
 first_seller AS (
     SELECT 
         ma.store_id,
+        ma.year_month_day_code,
         DATE(min(fs.completed_at)) AS first_seller_at
 FROM marketing ma
 LEFT JOIN qualified_orders fs ON ma.store_id = fs.store_id
 GROUP BY ma.store_id
 ),
+
 existing_data AS (
     {{ get_existing_data(this, ['store_id', 'sys_audit_created_on', 'sys_audit_created_by']) }}
 )
@@ -36,14 +42,14 @@ SELECT
     current_timestamp AS sys_audit_updated_on,
     'data-dev-dbt-products' AS sys_audit_updated_by
 FROM first_seller f
-LEFT JOIN existing_data e ON ma.store_id = e.store_id 
+LEFT JOIN existing_data e ON f.store_id = e.store_id 
 WHERE
     {% if not is_incremental() %}
-      sys_audit_updated_on >= DATE '2020-01-01'
+      first_seller_at >= DATE '2023-01-01'
     {% endif %}
     {% if is_incremental() %}
      first_seller_at > (
-        SELECT COALESCE(MAX(sys_audit_updated_on), DATE '2020-01-01')
+        SELECT COALESCE(MAX(first_seller_at), DATE '2023-01-01')
         FROM {{ this }}
       )
     {% endif %}
--- nubeproduct/models/intermediate/marketing/_int__first_seller_7_or_more_sales_90d.sql
+++ nubeproduct/models/intermediate/marketing/_int__first_seller_7_or_more_sales_90d.sql
@@ -1,5 +1,34 @@
+with 
+blocked_stores AS (
+    SELECT
+        related_id
+    FROM {{ source('int_moltres', 'mwp_tags') }} as tg
+    WHERE tg.type = 'store'
+        AND (tg.tag = 'sre-block-store-429'
+            OR tg.tag = 'sre-block-store-404')
+),
+
+paid_orders as (
+    select * from {{ ref('company_metrics_paid_orders') }}
+    WHERE store_id NOT IN (SELECT related_id FROM blocked_stores)
+),
+
+paid_orders_window as (
+    select
+        o1.store_id,
+        o1.completed_at,
+        count(o2.id) as sales_90d
+    from paid_orders o1
+    inner join paid_orders o2
+        on o1.store_id = o2.store_id
+        and o2.completed_at between dateadd(day, -90, o1.completed_at) and o1.completed_at
+    where o1.year_month_day_code >= 20230101
+      and o2.year_month_day_code >= 20230101
+    group by o1.store_id, o1.completed_at
+)
+
 select
     store_id,
     completed_at
-from {{ ref('_int__first_seller_orders_with_window') }}
+from paid_orders_window
 where sales_90d >= 7
