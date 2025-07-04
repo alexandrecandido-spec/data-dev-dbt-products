c0c6bb647be37acdbd7501e6be10bd94a8e16c68
Giselle Galli <giselle.galli@tiendanube.com>
2025-07-04 10:17:53 -0300

    create first seller

--- nubeproduct/models/data_product/marketing/first_seller_date.sql
+++ nubeproduct/models/data_product/marketing/first_seller_date.sql
@@ -15,12 +15,10 @@ WITH marketing AS (
     FROM {{ ref('marketing_attribution_model') }}
     WHERE year_month_day_code >= 20230101
 ),
-
 qualified_orders as (
     SELECT *
     FROM {{ ref('_int__first_seller_7_or_more_sales_90d') }}
 ),
-
 first_seller AS (
     SELECT 
         ma.store_id,
@@ -30,16 +28,16 @@ FROM marketing ma
 LEFT JOIN qualified_orders fs ON ma.store_id = fs.store_id
 GROUP BY ma.store_id,  ma.year_month_day_code
 ),
-
 existing_data AS (
     {{ get_existing_data(this, ['store_id', 'sys_audit_created_on', 'sys_audit_created_by']) }}
 )
-
 SELECT
-    f.*,
-    COALESCE(e.sys_audit_created_on, current_timestamp) AS sys_audit_created_on,
+    f.store_id, 
+    f.year_month_day_code,
+    f.first_seller_at,
+    COALESCE(e.sys_audit_created_on, current_timestamp()) AS sys_audit_created_on,
     COALESCE(e.sys_audit_created_by, 'data-dev-dbt-products') AS sys_audit_created_by,
-    current_timestamp AS sys_audit_updated_on,
+    current_timestamp() AS sys_audit_updated_on,
     'data-dev-dbt-products' AS sys_audit_updated_by
 FROM first_seller f
 LEFT JOIN existing_data e ON f.store_id = e.store_id 
@@ -48,8 +46,6 @@ WHERE
       first_seller_at >= DATE '2023-01-01'
     {% endif %}
     {% if is_incremental() %}
-     first_seller_at > (
-        SELECT COALESCE(MAX(first_seller_at), DATE '2023-01-01')
+     first_seller_at is not null
         FROM {{ this }}
-      )
     {% endif %}
