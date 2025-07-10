8f49bba96c66f4b2ff7d2d151ee754b7e13f5232
Felipe Lorenzo <felipe.lorenzo@tiendanube.com>
2025-07-10 16:22:35 -0300

    Adding int partners_info

---
+++ nubeproduct/models/intermediate/partners/_int_partners__partner_stores_partners_info.sql
@@ -0,0 +1,16 @@
+SELECT 
+    id AS partner_id,
+    code AS partner_code,
+    name AS partner_name, 
+    MC.country_code AS partner_country_code,
+    created_at AS partner_created_at_ts,
+FROM {{ source('int_ecosystem', 'mwp_partners') }} AS MP
+LEFT JOIN 
+    (
+        SELECT DISTINCT
+            id AS country_id,
+            code AS country_code,
+            name_en AS country_name
+        FROM {{ source('int_moltres', 'mwp_countries') }} 
+    ) AS MC
+ON MP.country = MC.country_id
