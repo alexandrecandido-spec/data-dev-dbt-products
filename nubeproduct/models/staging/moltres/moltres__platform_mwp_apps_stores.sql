8ddcc1ca0106c3e797ce0b3df2d81c3d69d9b4a4
Fernando <usuario@MacBook-Pro-de-admin.local>
2025-07-11 10:57:24 -0300

    modified queries to fix problems

--- nubeproduct/models/staging/moltres/moltres__platform_mwp_apps_stores.sql
+++ nubeproduct/models/staging/moltres/moltres__platform_mwp_apps_stores.sql
@@ -4,7 +4,7 @@
         unique_key=['installed_app_id'],
         incremental_strategy='merge',
         on_schema_change='fail',
-        tags=["product","daily-7am"]
+        tags=["product","daily-8am"]
     )
 }}
 
