c8abb54ac469c3e5417fd702f679bc945fdf30b8
Matias Scasso <matias.scasso@tiendanube.com>
2025-07-16 18:07:44 -0300

    testing query orders_b2b

--- nubeproduct/models/data_product/product/product_orders_b2b.sql
+++ nubeproduct/models/data_product/product/product_orders_b2b.sql
@@ -7,7 +7,7 @@
 SELECT
     b2b.id,
 	o.id as order_id,
-    si.id as store_id,
+    si.store_id,
     si.domain,
     si.country,
     si.state,
@@ -21,5 +21,5 @@ SELECT
     'data-dev-dbt-products' AS sys_audit_updated_by
 FROM {{ ref('orders__mwp_orders_b2b') }} b2b
 JOIN {{ ref('orders__mwp_orders') }} o on b2b.order_id = o.id
-JOIN {{ ref('moltres__mwp_store_info') }} si on si.id = o.store_id -- excludes stores with state 4
+JOIN {{ ref('moltres__mwp_store_info') }} si on si.store_id = o.store_id -- excludes stores with state 4
 LEFT JOIN {{ ref('operations_grouping_plans') }} gp on gp.plan = si.plan
