3f7d0911c2c859a90ce177b8db41f4b522edc2e8
Matias Scasso <matias.scasso@tiendanube.com>
2025-07-18 09:15:58 -0300

    updating orders_b2b

--- nubeproduct/models/data_product/product/product_orders_b2b.sql
+++ nubeproduct/models/data_product/product/product_orders_b2b.sql
@@ -1,6 +1,9 @@
 {{
     config(
-        tags=["daily-morning"]
+        tags=["daily-morning"],
+        materialized='incremental',
+        unique_key='id',
+        on_schema_change='fail'
     )
 }}
 
@@ -23,3 +26,13 @@ FROM {{ ref('orders__mwp_orders_b2b') }} b2b
 JOIN {{ ref('orders__mwp_orders') }} o on b2b.order_id = o.id
 JOIN {{ ref('moltres__mwp_store_info') }} si on si.store_id = o.store_id -- excludes stores with state 4
 LEFT JOIN {{ ref('operations_grouping_plans') }} gp on gp.plan = si.plan
+
+{% if is_incremental() %}
+
+WHERE
+    b2b.sys_audit_updated_on >= (select coalesce(max(a.sys_audit_updated_on),'1900-01-01') from {{ this }} a )
+    OR o.sys_audit_updated_on >= (select coalesce(max(a.sys_audit_updated_on),'1900-01-01') from {{ this }} a )
+    OR si.sys_audit_updated_on >= (select coalesce(max(a.sys_audit_updated_on),'1900-01-01') from {{ this }} a )
+    OR gp.sys_audit_updated_on >= (select coalesce(max(a.sys_audit_updated_on),'1900-01-01') from {{ this }} a )
+
+{% endif %}
--- nubeproduct/models/staging/orders/orders__mwp_orders_b2b.sql
+++ nubeproduct/models/staging/orders/orders__mwp_orders_b2b.sql
@@ -1,6 +1,9 @@
 {{
     config(
-        tags = ["product","daily-morning"]
+        tags = ["product","daily-morning"],
+        materialized='incremental',
+        unique_key='id',
+        on_schema_change='fail'
     )
 }}
 
@@ -14,5 +17,11 @@ SELECT
 	current_timestamp AS sys_audit_updated_on,
 	'data-dev-dbt-products' AS sys_audit_updated_by
 FROM
-	{{ source('stg_orders',
-	'mwp_orders_b2b') }}
+	{{ source('stg_orders', 'mwp_orders_b2b') }}
+
+{% if is_incremental() %}
+
+WHERE sys_audit_updated_on >= 
+(select coalesce(max(sys_audit_updated_on),'1900-01-01') from {{ this }} )
+
+{% endif %}
