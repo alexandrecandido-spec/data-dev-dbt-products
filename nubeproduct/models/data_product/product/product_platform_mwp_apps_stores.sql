ca15f7846672509f9cd2ac459eb431e13945b550
Fernando <usuario@ip-192-168-1-47.ec2.internal>
2025-07-21 13:30:28 -0300

    added new columns to dataproduct

--- nubeproduct/models/data_product/product/product_platform_mwp_apps_stores.sql
+++ nubeproduct/models/data_product/product/product_platform_mwp_apps_stores.sql
@@ -78,8 +78,8 @@ SELECT
     dfg.app_uninstall_date,
     current_timestamp as sys_admin_created_at,
     'data-dev-dbt-products' as sys_admin_creatd_by,
-    current_timestamp as sys_audit_updated_at
-    'data-dev-dbt-products' as sys_admin_updated_by,
+    current_timestamp as sys_audit_updated_at,
+    'data-dev-dbt-products' as sys_admin_updated_by
 FROM deduped_final_groups dfg
         {% if is_incremental() %}
     WHERE 
