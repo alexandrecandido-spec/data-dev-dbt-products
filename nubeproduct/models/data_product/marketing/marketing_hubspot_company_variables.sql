bab00bac305e91df5162fe2048b5b1290ab95a38
Santiago Gandolfo <santiago.gandolfo@tiendanube.com>
2025-08-07 13:46:13 -0300

    Update marketing_hubspot_company_variables.sql

--- nubeproduct/models/data_product/marketing/marketing_hubspot_company_variables.sql
+++ nubeproduct/models/data_product/marketing/marketing_hubspot_company_variables.sql
@@ -1,56 +1,40 @@
-{{ config(materialized="table", tags=["daily-6am"]) }}
+{{ config(
+    materialized="incremental",
+    unique_key="store_id",
+    incremental_strategy='merge',
+    tags=["daily-6am"]
+) }}
 
-with
-    existing_data as (
-        {{
-            get_existing_data(
-                this, ["store_id", "sys_audit_created_on", "sys_audit_created_by"]
-            )
-        }}
-    ),
-    dependency_audit_dates as (
-        select
-            merchant.store_id,
-            greatest(
-                merchant.sys_audit_updated_on,
-                coalesce(segment.sys_audit_updated_on, merchant.sys_audit_updated_on),
-                coalesce(vertical.sys_audit_updated_on, merchant.sys_audit_updated_on)
-            ) as max_dependency_audit_date
-        from {{ ref("dim_merchant_info") }} merchant
-        left join
-            {{ ref("dim_segment_type") }} segment
-            on segment.segment_id = merchant.current_segment_id
-        left join
-            {{ ref("dim_vertical_type") }} vertical
-            on vertical.vertical_id = merchant.vertical_id
-    ),
-    merchant_info as (
-        select
-            merchant.store_id,
-            coalesce(segment.segment_name, '') as status_by_order_str,
-            coalesce(vertical.vertical_name, '') as vertical_str,
-            audit_dates.max_dependency_audit_date
-        from {{ ref("dim_merchant_info") }} merchant
-        left join
-            {{ ref("dim_segment_type") }} segment
-            on segment.segment_id = merchant.current_segment_id
-        left join
-            {{ ref("dim_vertical_type") }} vertical
-            on vertical.vertical_id = merchant.vertical_id
-        left join
-            dependency_audit_dates audit_dates
-            on audit_dates.store_id = merchant.store_id
-    )
+WITH source_data AS (
+    SELECT
+        merchant.store_id,
+        coalesce(segment.segment_name, '') as status_by_order_str,
+        coalesce(vertical.vertical_name, '') as vertical_str
+    FROM {{ ref("dim_merchant_info") }} merchant
+    LEFT JOIN
+        {{ ref("dim_segment_type") }} segment
+        ON segment.segment_id = merchant.current_segment_id
+    LEFT JOIN
+        {{ ref("dim_vertical_type") }} vertical
+        ON vertical.vertical_id = merchant.vertical_id
+)
 
-select
+SELECT
     info.store_id,
     info.status_by_order_str,
     info.vertical_str,
-    coalesce(existing.sys_audit_created_on, current_timestamp) as sys_audit_created_on,
+    coalesce(existing_data.sys_audit_created_on, current_timestamp) as sys_audit_created_on,
     coalesce(
-        existing.sys_audit_created_by, 'data-dev-dbt-products'
+        existing_data.sys_audit_created_by, 'data-dev-dbt-products'
     ) as sys_audit_created_by,
-    info.max_dependency_audit_date as sys_audit_updated_on,
+    current_timestamp as sys_audit_updated_on,
     'data-dev-dbt-products' as sys_audit_updated_by
-from merchant_info info
-left join existing_data existing on info.store_id = existing.store_id
+FROM
+    source_data as info
+LEFT JOIN
+    {{ this }} as existing_data
+    ON info.store_id = existing_data.store_id
+WHERE
+    existing_data.store_id IS NULL OR
+    info.status_by_order_str <> existing_data.status_by_order_str OR
+    info.vertical_str <> existing_data.vertical_str
