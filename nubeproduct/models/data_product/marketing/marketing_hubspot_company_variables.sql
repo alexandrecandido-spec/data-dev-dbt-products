c397c31096970568111e2793e30310eb38a4a07c
Santiago Gandolfo <santiago.gandolfo@tiendanube.com>
2025-08-14 11:16:53 -0300

    Update marketing_hubspot_company_variables.sql

--- nubeproduct/models/data_product/marketing/marketing_hubspot_company_variables.sql
+++ nubeproduct/models/data_product/marketing/marketing_hubspot_company_variables.sql
@@ -2,7 +2,7 @@
     config(
         materialized="incremental",
         unique_key="store_id",
-        on_schema_change="fail",
+        on_schema_change="sync_all_columns",
         incremental_strategy="merge",
         tags=["daily-6am"],
     )
