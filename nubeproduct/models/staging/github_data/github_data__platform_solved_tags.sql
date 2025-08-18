5421ad9e0eb7870d62cde906c6c8db5cb8840db5
Fernando <usuario@ip-192-168-1-47.ec2.internal>
2025-08-18 16:41:49 -0300

    fixed incremental issues

--- nubeproduct/models/staging/github_data/github_data__platform_solved_tags.sql
+++ nubeproduct/models/staging/github_data/github_data__platform_solved_tags.sql
@@ -1,41 +1,36 @@
-{{
-    config(
-        materialized='incremental',
-        unique_key=['issue_number'],
-        incremental_strategy='merge',
-        on_schema_change='fail',
-        tags=["platform","daily-7am"]
-    )
-}}
+{{ config(
+    materialized = 'table',
+    on_schema_change = 'fail',
+    tags = ['daily']
+) }}
 
 with issue_labels as (
-select 
-    il.issue_number as issue_number
-    ,il.name as issue_label_name
-    ,case 
-        when il.name = 'Solved by app BR' then 'BR'
-        when il.name = 'Solved by app AR' then 'AR'
-        when il.name = 'Solved by app CL' then 'CL'
-        when il.name = 'Solved by app MX' then 'MX'
-        when il.name = 'Solved by app CO' then 'CO'
-    end as label_country
-    ,date(max(ie.github_created_at)) as creation_date
-from {{ source('stg_github_data', 'issue_label') }} il
-left join {{ source('stg_github_data', 'issue_events') }} ie 
-    on il.repo_name = ie.repo_name 
-    and il.issue_number = ie.issue_number 
-    and il.name = ie.content
-where true
-and il.name like '%Solved by app%'
+    select 
+        il.issue_number as issue_number,
+        il.name as issue_label_name,
+        case 
+            when il.name = 'Solved by app BR' then 'BR'
+            when il.name = 'Solved by app AR' then 'AR'
+            when il.name = 'Solved by app CL' then 'CL'
+            when il.name = 'Solved by app MX' then 'MX'
+            when il.name = 'Solved by app CO' then 'CO'
+        end as label_country,
+        date(max(ie.github_created_at)) as creation_date
+    from {{ source('stg_github_data', 'issue_label') }} il
+    left join {{ source('stg_github_data', 'issue_events') }} ie 
+        on il.repo_name = ie.repo_name 
+        and il.issue_number = ie.issue_number 
+        and il.name = ie.content
+    where il.name like '%Solved by app%'
+    group by il.issue_number, il.name
 )
 SELECT 
-    il.*
-    ,current_timestamp AS sys_audit_created_on
-    ,'data-dev-dbt-products' AS sys_audit_created_by
-    ,current_timestamp AS sys_audit_updated_on
-    ,'data-dev-dbt-products' AS sys_audit_updated_by 
+    il.issue_number,
+    il.issue_label_name,
+    il.label_country,
+    il.creation_date,
+    current_timestamp AS sys_audit_created_on,
+    'data-dev-dbt-products' AS sys_audit_created_by,
+    current_timestamp AS sys_audit_updated_on,
+    'data-dev-dbt-products' AS sys_audit_updated_by 
 FROM issue_labels il
-where
-{% if is_incremental() %}
-and il.sys_audit_updated_on >= (select coalesce(max(il.sys_audit_updated_on),'1900-01-01') from {{ this }} il)
-{% endif %}
