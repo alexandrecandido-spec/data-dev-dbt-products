a640023fbdc0b2065b3cae7020043260d0441301
Gustavo Oliveira <gustavo.oliveira@nuvemshop.com.br>
2025-08-20 11:09:05 -0300

    Creation of a standardization model for shipping options for shipments made via shipping apps

---
+++ nubeproduct/models/intermediate/logistics/_int_logistics_shipping__filtered_orders.sql
@@ -0,0 +1,12 @@
+SELECT
+      id
+    , store_id
+    , shipping_method
+    , shipping_option_code
+    , shipping_option
+    , year_month_code
+FROM 
+    {{ source('stg_orders', 'mwp_orders') }}
+    WHERE 1=1
+        AND year_month_code >= CAST(DATE_FORMAT(DATE_SUB(CURRENT_DATE(), 730), 'yyyyMM') AS INT)
+        AND shipping_method LIKE 'api_%'
