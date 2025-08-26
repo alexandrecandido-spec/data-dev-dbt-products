d77fe3536a8c1a2a4b4956c823069a8daf1fb7fa
Fernando <usuario@ip-192-168-1-93.ec2.internal>
2025-08-26 14:28:34 -0300

    changed logic in intermediate table to fix errors

--- nubeproduct/models/data_product/product/product_platform_development_issues.sql
+++ nubeproduct/models/data_product/product/product_platform_development_issues.sql
@@ -1,7 +1,7 @@
 {{ config(
     materialized = 'incremental',
     incremental_strategy = 'merge',
-    unique_key = ['issue_number', 'issue_country'],
+    unique_key = ['issue_number', 'labels_country'],
     partition_by = 'created_at',
     on_schema_change = 'fail',
     tags = ['product','daily-8am']
@@ -20,7 +20,7 @@ select
     ,i.title
     ,i.labels_tipo
     ,i.labels_domain
-    --,labels_country
+    ,labels_country
     ,i.issue_country
     ,i.store_country
     ,i.url
@@ -35,7 +35,7 @@ select
     ,i.is_dealbreaker
     ,i.is_high_impact
     ,i.comments
-    ,i.is_ticket_active
+    ,case when registered_month between date_trunc('month', i.created_at) and coalesce(i.closed_at,date('2100-01-01')) then true else false end as is_ticket_active
 from {{ ref('_int_pd_github_issues') }} i
 cross join {{ ref('_int_pd_github_dates') }} d
 where true
@@ -52,7 +52,7 @@ issues_final as (
         ,title
         ,labels_tipo
         ,labels_domain
-        --,labels_country
+        ,labels_country
         ,issue_country
         ,i.store_country
         ,url
@@ -61,38 +61,26 @@ issues_final as (
         ,has_product_dependent_tag
         ,has_app_improvement_tag
         ,has_new_app_or_integration_tag
+        /*
+        ,count(distinct i.store_id ) as total_stores
+        ,count(distinct case when is_dealbreaker then i.store_id end) as dealbreakers
+        ,count(distinct case when  is_high_impact then i.store_id end) as high_impact_stores
+        */
         ,count(distinct case when i.store_country = issue_country then i.store_id end) as total_stores
         ,count(distinct case when i.store_country = issue_country and is_dealbreaker then i.store_id end) as dealbreakers
         ,count(distinct case when i.store_country = issue_country and is_high_impact then i.store_id end) as high_impact_stores
         ,max(comments) as stock_comments
-        ,sum(gmv_local_currency_monthly) as impacted_gmv_local
+        ,sum(avg_gmv_lc_last_3months) as impacted_gmv_local
         ,sum(gmv_usd_monthly) as impacted_gmv_usd
         --,count(distinct case when is_relevant then i.store_id end) as plus_ones
     from issues_dates i
     left join {{ ref('_int__pd_stores_gmv') }} g
         on  i.store_id = g.store_id
         and i.registered_month = g.registered_month
-    group by
-        i.registered_month
-        ,i.issue_number
-        ,state
-        ,created_at
-        ,days_open
-        ,closed_at
-        ,title
-        ,labels_tipo
-        ,labels_domain
-        ,issue_country
-        ,i.store_country
-        ,url
-        ,labels
-        ,has_non_tech_enable_tag
-        ,has_product_dependent_tag
-        ,has_app_improvement_tag
-        ,has_new_app_or_integration_tag
+    group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
 )
 select 
-    concat(cast(p.ticket_number as string), p.issue_country) as unique_issue_country
+    concat(cast(p.ticket_number as string), p.labels_country) as unique_issue_country
     ,p.* 
     ,{% if is_incremental() %}
         coalesce(existing.sys_audit_created_on, current_timestamp) as sys_audit_created_on
@@ -106,5 +94,5 @@ from issues_final p
 {% if is_incremental() %}
 left join {{ this }} existing
     on p.ticket_number = existing.ticket_number 
-    and p.issue_country = existing.issue_country
+    and p.labels_country = existing.labels_country
 {% endif %}
--- nubeproduct/models/intermediate/product/_int_pd_github_issues.sql
+++ nubeproduct/models/intermediate/product/_int_pd_github_issues.sql
@@ -1,51 +1,83 @@
+with base as (
+    select *
+    from {{ ref('product_issues_and_problems_summary') }} i
+    where repo_name = 'issues'
+      and labels_tipo = 'App'
+),
+exploded as (
+    select
+        b.*,
+        trim('AR') as issue_country
+    from base b
+    where b.labels_country like '%AR%'
+    UNION ALL
+    select
+        b.*,
+        trim('BR') as issue_country
+    from base b
+    where b.labels_country like '%BR%'
+    UNION ALL
+    select
+        b.*,
+        trim('MX') as issue_country
+    from base b
+    where b.labels_country like '%MX%'
+    UNION ALL
+    select
+        b.*,
+        trim('CO') as issue_country
+    from base b
+    where b.labels_country like '%CO%'
+    UNION ALL
+    select
+        b.*,
+        trim('CL') as issue_country
+    from base b
+    where b.labels_country like '%CL%'
+)
 select distinct
-    i.repo_name
-    ,i.issue_number
-    ,state
-    ,created_at
-    ,case when state = 'open' then datediff(current_date, cast(created_at as date)) 
-        when state = 'closed' then datediff(cast(closed_at as date), cast(created_at as date))
-    end as days_open
-    ,closed_at
-    ,title
-    ,labels_tipo
-    ,trim(both ';' FROM concat_ws(';',labels_domain,
-        CASE 
-            WHEN labels LIKE '%Dropshipping App%' 
-                AND (labels_domain IS NULL OR labels_domain NOT LIKE '%Dropshipping App%') 
-            THEN 'Dropshipping App' END,
-        CASE 
-            WHEN labels LIKE '%Sales Channels App%' 
-                AND (labels_domain IS NULL OR labels_domain NOT LIKE '%Sales Channels App%') 
-            THEN 'Sales Channels App'END
-        )) 
-    AS labels_domain
-    --,labels_country
-    ,trim(countries) as issue_country
-    ,country as store_country
-    ,case when i.repo_name = 'problems' then concat('https://github.com/TiendaNube/Problems/issues/',cast(i.issue_number as string))
-        when i.repo_name = 'issues' then concat('https://github.com/TiendaNube/issues/issues/',cast(i.issue_number as string))
-    end as url
-    ,labels
-    ,case when lower(labels) like '%non tech enable%' then true else false end as has_non_tech_enable_tag
-    ,case when lower(labels) like '%product dependent%' then true else false end as has_product_dependent_tag
-    ,case when lower(labels) like '%app improvement%' then true else false end as has_app_improvement_tag
-    ,case when lower(labels) like '%New App or Integration%' then true else false end as has_new_app_or_integration_tag
-    ,i.store_id
+    repo_name,
+    issue_number,
+    state,
+    created_at,
+    case when state = 'open' then datediff(current_date, cast(created_at as date)) 
+         when state = 'closed' then datediff(cast(closed_at as date), cast(created_at as date))
+    end as days_open,
+    closed_at,
+    title,
+    trim(both ';' FROM concat_ws(';',labels_domain,
+        case when labels like '%Dropshipping App%' 
+              and (labels_domain is null or labels_domain not like '%Dropshipping App%') 
+             then 'Dropshipping App' end,
+        case when labels like '%Sales Channels App%' 
+              and (labels_domain is null or labels_domain not like '%Sales Channels App%') 
+             then 'Sales Channels App' end
+    )) as labels_domain,
+    labels_country,
+    labels_tipo,
+    issue_country,
+    country as store_country,
+    case when repo_name = 'problems' then concat('https://github.com/TiendaNube/Problems/issues/',cast(issue_number as string))
+         when repo_name = 'issues' then concat('https://github.com/TiendaNube/issues/issues/',cast(issue_number as string))
+    end as url,
+    labels,
+    case when lower(labels) like '%non tech enable%' then true else false end as has_non_tech_enable_tag,
+    case when lower(labels) like '%product dependent%' then true else false end as has_product_dependent_tag,
+    case when lower(labels) like '%app improvement%' then true else false end as has_app_improvement_tag,
+    case when lower(labels) like '%New App or Integration%' then true else false end as has_new_app_or_integration_tag,
+    store_id,
+    case when impact = 'dealbreaker' then true else false end as is_dealbreaker,
+    case when impact = 'high' then true else false end as is_high_impact,
+    comments
+from exploded
+
+/*
+Datos faltantes para agregar
     --,case when p.is_relevant is null then false else p.is_relevant end as is_relevant
     --,i.comment_date
-    ,case when i.impact = 'dealbreaker' then true else false end as is_dealbreaker
-    ,case when i.impact = 'high' then true else false end as is_high_impact
-    ,i.comments
-from {{ ref('product_issues_and_problems_summary') }} i
-CROSS JOIN UNNEST(SPLIT(labels_country, ';')) AS t (countries)
-/*
-left join plus_ones p
+    left join plus_ones p
     on i.issue_number = p.issue_number
     and i.repo_name = p.repo_name
     and i.store_id = p.store_id
     and i.issue_number = p.issue_number
 */
-where true
-and i.repo_name = 'issues'
-and labels_tipo = 'App'
