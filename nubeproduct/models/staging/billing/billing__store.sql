11b04174d276eed4e09eedee21a1b88c9e13c17c
Gustavo Braga <gustavobraga@ip-192-168-3-38.ec2.internal>
2025-09-16 09:38:39 -0300

    feat(finance): Create Staging Models for Billing System

--- nubeproduct/models/staging/billing/billing__store.sql
+++ nubeproduct/models/staging/billing/billing__store.sql
@@ -1,7 +1,7 @@
 {{
   config(
     materialized='incremental',
-    unique_key='payment_transaction_id',
+    unique_key='billing_store_id',
     on_schema_change='fail',
     tags=['finance', 'daily-7am']
   )
@@ -9,33 +9,29 @@
 
 WITH source_data AS (
     SELECT
-        id AS payment_transaction_id,
-        payorderid AS pay_order_id,
-        paymentmethodid AS payment_method_id,
-        status,
-        CAST(paidat AS TIMESTAMP) AS paid_at,
+        id AS billing_store_id,
+        coreid AS store_id,
+        countryid AS country_id,
         sys_audit_created_on AS source_sys_audit_created_on,
         sys_audit_created_by AS source_sys_audit_created_by,
         updatedat AS updated_at,
-        CAST(date_format(paidat, 'yyyyMMdd') AS INT) AS year_month_day_code
+        CAST(date_format(sys_audit_created_on, 'yyyyMMdd') AS INT) AS year_month_day_code
     FROM
-        {{ source('stg_billing', 'payment_transaction') }}
+        {{ source('stg_billing', 'store') }}
+
     {% if is_incremental() %}
     WHERE updatedat >= (SELECT COALESCE(MAX(updated_at), '1900-01-01') - INTERVAL '1 hour' FROM {{ this }})
     {% endif %}
 ),
 
 existing_data AS (
-    {{ get_existing_data(this, ['payment_transaction_id', 'sys_audit_created_on', 'sys_audit_created_by']) }}
+    {{ get_existing_data(this, ['billing_store_id', 'sys_audit_created_on', 'sys_audit_created_by']) }}
 )
 
 SELECT
-    s.payment_transaction_id,
-    s.pay_order_id,
-    s.pay_order_id as paid_order_id,
-    s.payment_method_id,
-    s.status,
-    s.paid_at,
+    s.billing_store_id,
+    s.store_id,
+    s.country_id,
     s.year_month_day_code,
     s.updated_at,
     COALESCE(e.sys_audit_created_on, s.source_sys_audit_created_on, current_timestamp) AS sys_audit_created_on,
@@ -45,4 +41,4 @@ SELECT
 FROM
     source_data s
 LEFT JOIN
-    existing_data e ON s.payment_transaction_id = e.payment_transaction_id
+    existing_data e ON s.billing_store_id = e.billing_store_id
