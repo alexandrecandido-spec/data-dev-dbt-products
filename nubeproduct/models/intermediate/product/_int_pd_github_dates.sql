bb6cbec336d2bc73698e423af8df41f1e989b55d
Fernando <usuario@ip-192-168-32-31.ec2.internal>
2025-09-22 09:32:33 -0300

    changed dataproduct logic based on business desision

--- nubeproduct/models/intermediate/product/_int_pd_github_dates.sql
+++ nubeproduct/models/intermediate/product/_int_pd_github_dates.sql
@@ -1,3 +1,6 @@
-    select distinct
-        date_trunc('month', datemonth) as registered_month
-    from {{ ref('company_metrics_gmv_and_segments') }} 
+    SELECT distinct 
+        date_trunc('month',first_day_of_month) as registered_month
+    FROM refined.data_dimensions.dim_calendar
+    where date(first_day_of_month) 
+        between date_add('month', -24, date_Trunc('month',current_date)) 
+        and date_Trunc('month',current_date)
