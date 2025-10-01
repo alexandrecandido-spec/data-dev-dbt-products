79c92e0e9eb0ef7d94cc0f44aa176de0468d633f
Santiago Gandolfo <santiago.gandolfo@tiendanube.com>
2025-10-01 15:58:47 -0300

    Added support DAG

---
+++ dags/src/builders/support/support_dags.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.models import Variable
+import ast
+from datetime import datetime
+from src.core.run_dbt_custom import create_dbt_dag
+from src.core.utils.slack_manager import task_fail_slack_alert_bi
+from functools import partial
+
+SLACK_IDS= ast.literal_eval(Variable.get('slack_ids_analytics_engineer_alert'))
+
+default_args = {
+    'owner': 'santiago.gandolfo@tiendanube.com',
+    'depends_on_past': False,
+    'start_date': datetime(2025, 5, 8),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'on_failure_callback': partial(task_fail_slack_alert_bi,slack_ids=SLACK_IDS)
+}
+
+# Crear el DAG frecuencia diaria 4:30am
+dag = create_dbt_dag(
+    dag_id='dbt_support_daily-4_30am',
+    schedule_interval_tag='daily-4_30am',
+    default_args=default_args,
+    tags=['support', 'daily-4_30am']
+) 
