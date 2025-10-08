fe235674abebda590897ebc6b106dd077f74357e
solAtTiendanube <sol.abelleyro@tiendanube.com>
2025-10-08 17:25:18 -0300

    New data product product_catalog_features (#295)
    * Saving progress before merge
    
    * created data product and ran initial materialization in testing_product, tests passed
    
    * added gmv and order count - also removed hidden variant feature metrics that will be deprecated
    
    * added metafields counts and booleans
    
    * added churned date, limited admin/app use metrics to 3 months, casted dates
    
    * Empty commit to trigger status checks of PR number 295
    
    * Removed distinct in intermediate layer
    
    * creating raw catalog category table in staging WIP
    
    * Created product categories table in staging, removed it as a source for intermediate and changed references. Started doing the same for locations
    
    * added inventory levels and locations to staging and removed inventory levels from int. Location is being used. WIP
    
    * Created staging tables for the catalog domain, modified the intermediate queries to use the new staging tables

---
+++ nubeproduct/models/staging/product/product__mwp_product_videos.sql
@@ -0,0 +1,47 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='id',
+        on_schema_change='fail',
+        tags=["product","daily-10am"]
+    )
+}}
+with product_videos as (
+    SELECT 
+    year_month_code,
+    id,
+    order,
+    store_id,
+    product_id,
+    video_id,
+    created_at,
+    updated_at,
+    deleted_at
+    FROM {{ source('stg_catalog', 'mwp_product_videos') }} AS mil
+    {% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    -- (uses >= to include records whose timestamp occurred since the last run of this model)
+    -- (If event_time is NULL or the table is truncated, the condition will always be true and load all records)
+    where sys_audit_updated_on >= (select coalesce(max(sys_audit_updated_on),'1900-01-01') from {{ this }} )
+    {% endif %}
+),
+existing_data AS (
+    {{ get_existing_data(this, ['id', 'sys_audit_created_on', 'sys_audit_created_by']) }}
+)
+
+SELECT 
+    year_month_code,
+    product_videos.id,
+    order,
+    store_id,
+    product_id,
+    video_id,
+    created_at,
+    updated_at,
+    deleted_at,
+    COALESCE(e.sys_audit_created_on, current_timestamp) AS sys_audit_created_on,
+    COALESCE(e.sys_audit_created_by, 'data-dev-dbt-products') AS sys_audit_created_by,
+    current_timestamp AS sys_audit_updated_on,
+    'data-dev-dbt-products' AS sys_audit_updated_by
+FROM product_videos
+LEFT JOIN existing_data e ON product_videos.id = e.id
