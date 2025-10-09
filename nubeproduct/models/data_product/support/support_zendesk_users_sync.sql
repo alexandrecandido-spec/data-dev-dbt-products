b9bb7c915c38c8458b95872b99b997e89481d0a8
Sergio Regalado Alessi <sergio.alessi@tiendanube.com>
2025-10-09 17:28:20 -0300

    Replicate support_zendesk_partners_sync to support_zendesk_users_sync

--- nubeproduct/models/data_product/support/support_zendesk_users_sync.sql
+++ nubeproduct/models/data_product/support/support_zendesk_users_sync.sql
@@ -1,6 +1,9 @@
 {{
     config(
-        materialized="table",
+        materialized="incremental",
+        unique_key="user_id",
+        on_schema_change="sync_all_columns",
+        incremental_strategy="merge",
         tags=["daily-4_30am"],
         partition_by=["year_month_day_code"],
     )
@@ -29,9 +32,33 @@ select
     info.has_store,
     info.user_is_partner,
     info.partner_id,
-    current_timestamp as sys_audit_created_on,
-    'data-dev-dbt-products' as sys_audit_created_by,
+    {% if is_incremental() %}
+        coalesce(
+            existing_data.sys_audit_created_on, current_timestamp
+        ) as sys_audit_created_on,
+        coalesce(
+            existing_data.sys_audit_created_by, 'data-dev-dbt-products'
+        ) as sys_audit_created_by,
+    {% else %}
+        current_timestamp as sys_audit_created_on,
+        'data-dev-dbt-products' as sys_audit_created_by,
+    {% endif %}
     current_timestamp as sys_audit_updated_on,
     cast(date_format(current_timestamp, 'yyyyMMdd') as int) as year_month_day_code,
     'data-dev-dbt-products' as sys_audit_updated_by
 from source_data as info
+{% if is_incremental() %}
+    left join
+        {{ this }} as existing_data on info.user_id = existing_data.user_id
+        {% set monitored_cols = [
+            "email",
+            "name",
+            "store_id",
+            "phone",
+        ] %}
+    where
+        existing_data.user_id is null
+        {%- for col in monitored_cols %}
+            or (info.{{ col }} is distinct from existing_data.{{ col }})
+        {%- endfor %}
+{% endif %}
