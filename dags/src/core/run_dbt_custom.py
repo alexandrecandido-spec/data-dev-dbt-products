8482f7d37536bf9c5dfaac83040d159a830630a5
rodrint <140170172+rodrint@users.noreply.github.com>
2025-10-14 07:54:06 -0300

    fix paid orders bad call (#345)

--- dags/src/core/run_dbt_custom.py
+++ dags/src/core/run_dbt_custom.py
@@ -53,7 +53,9 @@ def create_dbt_dag(
     """
     
     """Continuar sumando condiciones de acuerdo a los schedules/tags"""
-    if schedule_interval_tag == 'daily-4am':
+    if schedule_interval_tag == 'daily-2am':
+        schedule_interval='0 2 * * *'
+    elif schedule_interval_tag == 'daily-4am':
         schedule_interval='0 4 * * *'
     elif schedule_interval_tag == 'daily-4_30am':
         schedule_interval = '30 4 * * *'
