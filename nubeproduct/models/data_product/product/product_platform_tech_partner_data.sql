1f78fd06e0a860f5c7dba8cfac389bd6711793d1
Fernando Martinez <fernando.martinez@tiendanube.com>
2025-10-17 09:28:20 -0300

    fixed dataproduct table names

--- nubeproduct/models/data_product/product/product_platform_tech_partner_data.sql
+++ nubeproduct/models/data_product/product/product_platform_tech_partner_data.sql
@@ -11,7 +11,7 @@ with
 partner_dates as (
     select
         *
-    from {{ ref('_int_pd_tech_partners_dates') }}
+    from {{ ref('_int_platform_tech_partners_dates') }}
 ),
 apps as (
    select
@@ -25,7 +25,7 @@ apps as (
         ,month_creation_date
         ,is_app_published
         ,LISTAGG(app_published_country , ', ') within group (order by app_published_country) as app_published_countries
-   from {{ ref('ecosystem_apps') }}
+   from {{ ref('ecosystem__apps') }}
    where app_deleted_date is null
    group by 1,2,3,4,5,6,7,8,9
 ),
@@ -43,13 +43,13 @@ total_installs as(
       ,app_id
       ,total_installs
       ,total_installs_active
-   from {{ ref('_int_pd_tech_partner_installs') }}
+   from {{ ref('_int_platform_tech_partner_installs') }}
 ),
 /*
 referrals as (
    SELECT
       *
-   from {{ ref('_int_pd_tech_partners_referrals') }}
+   from {{ ref('_int_platform_tech_partners_referrals') }}
 ),
 */
 partners as (
