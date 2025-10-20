7c4c92b4ab9abab8718fe359b6c8ea679fc8de29
Malen Correa Mulet <MalenCorreaMulet@MacBook-Pro-de-usuario.local>
2025-10-20 15:27:15 -0300

    actualiza query de GMV por marketing source

--- nubeproduct/models/data_product/marketing/marketing_gmv_by_mkt_source.sql
+++ nubeproduct/models/data_product/marketing/marketing_gmv_by_mkt_source.sql
@@ -1,19 +1,21 @@
 {{ config(
   materialized='incremental',
   unique_key=['store_id','completed_at'],
+  incremental_strategy='merge',
   on_schema_change='fail',
   partition_by='year_month_day_code',
   tags=['daily-9am']
 ) }}
  SELECT 
   gmv.*  
-  , current_timestamp sys_audit_created_on
-  , 'data-dev-dbt-products' sys_audit_created_by
-  , current_timestamp sys_audit_updated_on
-  , 'data-dev-dbt-products' sys_audit_updated_by
+  , current_timestamp as sys_audit_created_on
+  , 'data-dev-dbt-products' as sys_audit_created_by
+  , current_timestamp as sys_audit_updated_on
+  , 'data-dev-dbt-products' as sys_audit_updated_by
   FROM {{ref ('_int_marketing__daily_gmv_by_store')}} gmv
-  where gmv.year_month_day_code = '20250930'
+  where 1=1
   {% if is_incremental() %}
-    WHERE gmv.year_month_day_code > (SELECT MAX(year_month_day_code) FROM {{ this }})
+    AND gmv.completed_at >= 
+      (SELECT COALESCE(MAX(sys_audit_updated_on), '2000-01-01') FROM {{ this }}) 
   {% endif %}
                                             
