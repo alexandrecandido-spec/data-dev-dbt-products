aa8c4e01eed8c0ac6c73fc78a18457d57e93e2c6
Felipe Lorenzo <felipe.lorenzo@tiendanube.com>
2025-10-23 15:58:02 -0300

    Finishing dp

--- nubeproduct/models/intermediate/partnerships/_int_partnerships__partners_commissions_construction.sql
+++ nubeproduct/models/intermediate/partnerships/_int_partnerships__partners_commissions_construction.sql
@@ -1,27 +1,24 @@
 WITH commissions AS
 (
   SELECT 
-    id AS commission_id,
+    PL.id AS commission_id,
     partners_id AS partner_id,
     store_id,
     partner_ledger_entry_type_id,
-    IF(partner_ledger_entry_type_id = 5,'Withdrawal','Deposit') AS transaction_type,
+    PLET.name AS partner_ledger_entry_type_name,
+    IF(partner_ledger_entry_type_id IN(5,7),'Withdrawal','Deposit') AS transaction_type,
     CASE 
       WHEN partner_ledger_entry_type_id = 1 
       THEN 'affiliates'
       WHEN partner_ledger_entry_type_id = 6 
       THEN 'store_development'
-      WHEN partner_ledger_entry_type_id = 2 
+      WHEN partner_ledger_entry_type_id IN(2,8) 
       THEN 'apps'
       WHEN partner_ledger_entry_type_id = 3 
       THEN 'initial_balance'
       WHEN partner_ledger_entry_type_id = 4 
       THEN 'partner_bonus'
-      WHEN partner_ledger_entry_type_id = 7 
-      THEN 'core_payout'
-      WHEN partner_ledger_entry_type_id = 8 
-      THEN 'app_transaction'
-      ELSE 'Withdrawal'
+      ELSE 'payout'
     END AS commission_type,
     DATE(paid_at) AS commission_paid_date,
     DATE(created_at) AS commission_created_date,
@@ -29,7 +26,7 @@ WITH commissions AS
     original_currency AS store_currency,
     currency_exchange_rate,
     CASE 
-      WHEN partner_ledger_entry_type_id IN(2,3,4,5) 
+      WHEN partner_ledger_entry_type_id IN(2,3,4,5,7,8) 
       THEN ABS(credit_value)
       ELSE partner_commission_value
     END AS commission_amount,
@@ -37,9 +34,17 @@ WITH commissions AS
     partner_commission_percentage,
     plan_id,
     plan_value,
-    sys_audit_updated_on AS partner_ledger_change_timestamp
-  FROM {{ source('int_ecosystem', 'partner_ledger') }}
-  WHERE DATE(created_at) >= '2024-01-01'
-  --AND partner_ledger_entry_type_id IN(1,2,5,6)
+    DATE
+    (
+      GREATEST
+        (
+          PL.sys_audit_updated_on, 
+          PLET.sys_audit_updated_on
+        )
+    ) AS partner_ledger_change_timestamp
+  FROM {{ source('int_ecosystem', 'partner_ledger') }} AS PL
+  LEFT JOIN {{ source('int_ecosystem', 'partner_ledger_entry_type') }} AS PLET
+    ON PL.partner_ledger_entry_type_id = PLET.id
+  WHERE DATE(PL.created_at) >= '2024-01-01'
 )
 SELECT * FROM commissions
