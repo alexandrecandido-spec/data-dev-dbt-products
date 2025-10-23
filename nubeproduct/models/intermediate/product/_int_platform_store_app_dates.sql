0fedff0f840c0c927c0f44b8929e610e7a09fbbd
Fernando Martinez <fernando.martinez@tiendanube.com>
2025-10-23 15:33:02 -0300

    added new app monthly active user dataproduct

---
+++ nubeproduct/models/intermediate/product/_int_platform_store_app_dates.sql
@@ -0,0 +1,118 @@
+with 
+stores as (
+    select
+        s.store_id
+        ,domain as merchant_name
+        ,country_code as country
+        ,mail
+        ,phone
+        ,vertical_name as store_vertical
+        ,group_name
+        ,date(created_at) as creation_date
+        ,date(first_payment) as first_payment
+from {{ ref('company_metrics_merchant_info') }} s
+left join {{ ref('moltres__mwp_store_settings') }} ss
+  on s.store_id = ss.store_id
+)
+store_dates as (
+    select 
+        d.registered_month
+        ,p.*
+    from {{ ref('store_info') }} p
+    cross join {{ ref('_int_pd_github_dates') }} d
+),
+orders as (
+    SELECT
+        date(date_trunc('month', completed_at)) as registered_month
+        ,store_id
+        ,count(distinct id) as total_orders
+        ,sum(total) as total_gmv_local_currency
+        ,sum(total_in_usd) as total_gmv_usd
+    from {{ ref('company_metrics_paid_orders') }}
+),
+app_orders as (
+    select
+        date_trunc('month',registered_date) as registered_month
+        ,store_id
+        ,app_id
+        ,app_name
+        ,app_category
+        ,sum(total_orders) as total_orders
+        ,sum(total_gmv_local_currency) as total_gmv_local_currency
+        ,sum(total_gmv_usd) as total_gmv_usd
+    from {{ ref('g__platform__app_orders__agg_daily') }}
+),
+installs as (
+    SELECT
+        store_id
+        ,app_id
+        ,min(app_install_date) as app_install_date
+        ,max(app_uninstall_date) as app_uninstall_date
+    from {{ ref('product_platform_mwp_apps_stores') }}
+    group by 1,2
+),
+apps as (
+    SELECT
+        app_id
+        ,app_name
+        ,app_category
+        ,app_creation_date
+        ,app_published_date
+        ,app_deleted_date
+        ,is_app_published
+    from {{ ref('product__ecosystem__apps__scd') }}
+),
+segments as (
+    SELECT
+    date_trunc('month',datemonth) as registered_month
+    ,store_id
+    ,segment
+from {{ ref('company_metrics_gmv_and_segments') }}
+),
+scripts as (
+    SELECT
+        store_id
+        ,app_id
+        ,date(created_at) as created_date
+        ,date(deleted_at) as deleted_date
+    from {{ ref('product__ecosystem__mwp_scripts__scd') }}
+),
+shipping_carriers as (
+    SELECT
+        store_id
+        ,app_id
+        ,status
+        ,date(created_at) as created_date
+        ,date(deleted_at) as deleted_date
+    from {{ ref('product__ecosystem__mwp_shipping_carriers__scd') }}
+)
+SELECT distinct
+        s.*
+        ,a.app_id
+        ,app_name
+        ,app_category
+        ,is_published
+        ,app_install_date
+        ,app_uninstall_date
+        ,case when a.app_id is null then 1 else 0 end as has_no_apps
+        ,case when s.registered_month between date_trunc('month', app_install_date) and coalesce('2100-01-01', app_uninstall_date) then 1 else 0 end as is_app_active
+        ,case when s.registered_month = date_trunc('month', app_install_date) then 1 else 0 end as is_new_app_install
+        ,case when s.registered_month = date_trunc('month', app_uninstall_date) then 1 else 0 end as is_app_churn
+        ,sc.created_date as script_creation_date
+        ,sc.deleted_date as script_deletion_date
+        ,case when s.registered_month = date_trunc('month', sc.created_date) then 1 else 0 end as is_new_scrtip
+        ,case when s.registered_month = date_trunc('month', sc.deleted_date) then 1 else 0 end as is_churn_scrtip
+        ,case when s.registered_month between date_trunc('month', sc.created_date) and coalesce('2100-01-01',sc.deleted_date) then 1 else 0 end as is_script_active
+        ,sh.creation_date as shipping_carrier_creation_date
+        ,sh.deletion_date as shipping_carrier_deletion_date
+        ,sh.status as shipping_carrier_status
+        ,case when sh.status = 1 and s.registered_month between date_trunc('month', sh.creation_date) and coalesce('2100-01-01',sh.deletion_date) then 1 else 0 end as is_shipping_carrier_active
+from store_dates s
+left join installs a
+    on s.store_id = a.store_id
+left join scripts sc
+    on s.store_id = sc.store_id
+    and a.app_id = sc.app_id
+left join shipping_carriers sh
+    on s.store_id = sh.store_id
+    and a.app_id = sh.app_id
---
+++ nubeproduct/models/staging/ecosystem/product__ecosystem__mwp_scripts__scd.sql
@@ -0,0 +1,41 @@
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'merge',
+    unique_key = ['unique_store_app_id'],
+    on_schema_change = 'fail',
+    tags = ['product','daily-8am']
+) }}
+
+with 
+scripts as (
+      select distinct
+         store_id
+         ,app_id
+         ,event
+         ,"where" as source_event
+         ,date(created_at) as created_date
+         ,date(deleted_at) as deleted_date
+         ,rank() over(partition by app_id,store_id order by id desc) as script_rank
+      from {{ source('bronze_risk_ecommerce', 'mwp_scripts') }} s
+),
+scripts_final as (
+select *
+from scripts
+where script_rank = 1
+)
+SELECT
+    concat(cast(store_id as string), '_', cast(app_id as string)) as unique_store_app_id
+    ,s.*
+    ,current_timestamp AS sys_audit_created_on
+    ,'data-dev-dbt-products' AS sys_audit_created_by
+    ,current_timestamp AS sys_audit_updated_on
+    ,'data-dev-dbt-products' AS sys_audit_updated_by
+from scripts_final s
+{% if is_incremental() %}
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM {{ this }} existing 
+    WHERE existing.store_id = s.store_id
+    AND existing.app_id = s.app_id
+)
+{% endif %}
