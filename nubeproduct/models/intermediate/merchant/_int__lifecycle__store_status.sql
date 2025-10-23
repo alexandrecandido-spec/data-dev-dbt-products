fd52df213a1f0699857bc88c221f46bbb252d03a
Ronald Corcho <ronald.corcho@tiendanube.com>
2025-10-22 23:44:28 -0500

    Create s__lifecycle__store_status__ref model, also improve logical model of s__attributes__store_core__ref

---
+++ nubeproduct/models/intermediate/merchant/_int__lifecycle__store_status.sql
@@ -0,0 +1,58 @@
+WITH store_source AS (
+  SELECT
+    si.store_id
+    , si.created_at
+    , si.first_payment
+    , si.churned_at
+    , si.plan_id AS current_plan_id
+    , si.state
+    , si.disabled
+    , si.custom_theme
+    , si.sys_audit_updated_on
+  FROM {{ ref('merchant__attributes__store_info__ref') }} si
+), 
+segment_info AS (
+  SELECT
+    l.store_id
+    ,lower(ds.segment_name) as current_segment
+    ,CASE WHEN lower(ds.segment_name) IN ('no-seller', 'struggling-seller') THEN 'no-seller' ELSE 'seller' END AS is_seller
+    ,lower(ds.segment_name) as max_segment
+    ,l.sys_audit_updated_on
+  FROM {{ ref('_int_dim_merchant_info__segment') }} l
+  LEFT JOIN {{ ref('dimension__attributes__segment_type__ref') }} ds ON l.current_segment_id = ds.segment_id
+),
+blocked_store__info AS (
+  SELECT
+    bl.store_id
+    , bl.blocked_at
+    , bl.blocked_reason
+    , bl.blocked_last_updated_at
+  FROM {{ ref('merchant__attributes__store_tags__ref') }} bl
+  WHERE bl.blocked_reason is not null
+)
+
+
+SELECT 
+    ss.store_id
+    , ss.created_at
+    , ss.first_payment
+    , ss.churned_at
+    , fs.first_seller_at
+    , ss.current_plan_id
+    , pl.namev2 AS current_plan_name
+    , pl.grupo AS current_plan_type
+    , si.current_segment
+    , si.is_seller
+    , si.max_segment
+    , CASE WHEN bl.store_id IS NOT NULL THEN TRUE ELSE FALSE END AS is_store_blocked
+    , bl.blocked_reason
+    , bl.blocked_at
+    , ss.state
+    , ss.disabled
+    , ss.custom_theme
+    , greatest(ss.sys_audit_updated_on, si.sys_audit_updated_on, bl.blocked_last_updated_at, fs.sys_audit_updated_on) as change_timestamp
+FROM store_source ss 
+LEFT JOIN segment_info si ON ss.store_id = si.store_id
+LEFT JOIN blocked_store__info bl ON ss.store_id = bl.store_id
+LEFT JOIN {{ ref('s__general__grouping_plans__ref') }} pl ON ss.current_plan_id = pl.plan
+LEFT JOIN {{ ref('s__lifecycle__first_seller_date__ref') }} fs ON ss.store_id = fs.store_id
