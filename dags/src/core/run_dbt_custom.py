f9c82ab87fdc9884b7f44a0549b664500779aab7
Barbi <167924360+datansbam@users.noreply.github.com>
2025-10-24 13:02:47 -0300

    Changing DAGs, adding documentation and removing a data product (#399)
    * Changing DAGs, adding documentation and removing a data product
    
    * Adding dag file
    
    * Adding schedule to run_dbt_custom

--- dags/src/core/run_dbt_custom.py
+++ dags/src/core/run_dbt_custom.py
@@ -55,6 +55,8 @@ def create_dbt_dag(
     """Continuar sumando condiciones de acuerdo a los schedules/tags"""
     if schedule_interval_tag == 'daily-2am':
         schedule_interval='0 2 * * *'
+    elif schedule_interval_tag == 'daily-3am':
+        schedule_interval='0 3 * * *'
     elif schedule_interval_tag == 'daily-4am':
         schedule_interval='0 4 * * *'
     elif schedule_interval_tag == 'daily-4_30am':
