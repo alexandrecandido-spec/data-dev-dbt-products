d8236fb7d51822fb21d0727c52d270784706e711
Felipe Lorenzo <felipe.lorenzo@tiendanube.com>
2025-10-28 15:35:03 -0300

    Readapting restriction logic

--- nubeproduct/models/intermediate/cx/_int_cx__zendesk_macros__info.sql
+++ nubeproduct/models/intermediate/cx/_int_cx__zendesk_macros__info.sql
@@ -19,19 +19,22 @@ WITH actions AS (
       LATERAL VIEW POSEXPLODE(
         FROM_JSON(actions, 'array<struct<field:string,value:string>>')
       ) ex AS pos, item
-      UNION ALL 
-      SELECT
-        t.id,                                -- cambia por tu PK
-        ex.pos,
-        ex.item.field  AS field,
-        ex.item.value  AS value
-      FROM {{ source("int_zendesk_support_prod", "macros") }} t
-      LATERAL VIEW POSEXPLODE(
-        FROM_JSON(restriction, 'array<struct<field:string,value:string>>')
-      ) ex AS pos, item
     )
-  WHERE field IN('comment_mode_is_public','comment_value_html','current_tags','status','type','id','ids')
+  WHERE field IN('comment_mode_is_public','comment_value_html','current_tags','status')
   GROUP BY id
+),
+restrictions AS 
+(
+  SELECT
+    t.id,
+    r.type AS restriction_type,
+    r.id AS restriction_id,
+    r.ids AS restriction_ids
+  FROM {{ source("int_zendesk_support_prod", "macros") }} t
+  LATERAL VIEW
+    json_tuple( t.restriction, 'type', 'id') jt AS type_str, id_str
+  LATERAL VIEW
+    POSEXPLODE( ARRAY( FROM_JSON(t.restriction, 'struct<type:string,id:bigint,ids:array<bigint>>') ) ) p AS _pos, r
 )
 SELECT
   macros.id AS macro_id,
@@ -41,12 +44,13 @@ SELECT
   created_at AS macro_created_at,
   updated_at AS macro_updated_at,
   description AS macro_description,
-  a.comment_mode_is_public,
+  IF(a.comment_mode_is_public IS NULL, FALSE, a.comment_mode_is_public) AS comment_mode_is_public,
   a.comment_value_html,
   a.current_tags,
   a.status,
-  a.restriction_type,
-  a.restriction_id,
-  a.restriction_ids
+  r.restriction_type,
+  r.restriction_id,
+  r.restriction_ids
 FROM {{ source("int_zendesk_support_prod", "macros") }} AS macros
 LEFT JOIN actions AS a ON a.id = macros.id
+LEFT JOIN restrictions AS r ON r.id = macros.id
