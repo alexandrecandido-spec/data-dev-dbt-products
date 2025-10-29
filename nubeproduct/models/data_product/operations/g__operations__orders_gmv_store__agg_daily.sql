5bc7384f377f3596560c40968d8b602021d04536
Gustavo Braga <gustavobraga@MacBook-Pro-de-Admin-3.local>
2025-10-29 11:20:06 -0300

    ajustes em on_schema_change, simplificação do código e teste de incrementalidade. Imagens do teste em comentário

--- nubeproduct/models/data_product/operations/g__operations__orders_gmv_store__agg_daily.sql
+++ nubeproduct/models/data_product/operations/g__operations__orders_gmv_store__agg_daily.sql
@@ -1,18 +1,18 @@
-{{
-    config(
-        materialized        = "incremental",
-        incremental_strategy= "merge",
-        on_schema_change    = "sync_all_columns",
-        tags                = ["daily-9am-9pm"],
-        partition_by        = "year_month_day_code",
-        unique_key          = "row_key"
-    )
-}}
+{{ config(
+    materialized        = "incremental",
+    incremental_strategy= "merge",
+    partition_by        = "year_month_day_code",
+    unique_key          = "row_key",
+    on_schema_change    = "fail",
+    tags                = ["daily-9am-9pm"]
+) }}
 
+WITH existing_data AS (
+    {{ get_existing_data(this, ['row_key', 'orders', 'gmv', 'sys_audit_created_on', 'sys_audit_created_by']) }}
+),
 
-with src as (
-    select
-        -- Dimensões da Loja
+base_data AS (
+    SELECT
         o.store_id,
         o.country,
         o.domain,
@@ -25,8 +25,6 @@ with src as (
         o.nice_9_name,
         o.plan_group,
         o.bu,
-
-        -- Dimensões do Pedido (por dia)
         o.date,
         o.year_month_day_code,
         o.mes,
@@ -42,158 +40,58 @@ with src as (
         o.social_network,
         o.source_details,
         o.source_type,
-
-        -- Métricas
         o.gmv,
         o.gmv_usd,
         o.orders,
         o.orders_free_shipping,
-        o.products
-    from {{ ref('_int__operations__orders_gmv_store__agg_daily_prep') }} o
-),
-
-src_with_key as (
-    select
-        -- hash (varchar) em Trino: lower(hex(md5(...)))
+        o.products,
         lower(hex(md5(concat_ws(
             '|',
-            cast(store_id as varchar(10)),
-            cast(country as varchar(10)),
-            coalesce(domain, ''),
-            coalesce(vertical, ''),
-            coalesce(province, ''),
-            coalesce(city, ''),
-            coalesce(region, ''),
-            coalesce(business_size, ''),
-            coalesce(segment, ''),
-            coalesce(nice_9_name, ''),
-            coalesce(plan_group, ''),
-            coalesce(bu, ''),
-            cast(date as varchar(10)),
-            cast(year_month_day_code as varchar(10)),
-            cast(mes as varchar(10)),
-            coalesce(platform_type, ''),
-            coalesce(storefront, ''),
-            coalesce(device, ''),
-            coalesce(payment_provider, ''),
-            coalesce(payment_method, ''),
-            coalesce(shipping_method, ''),
-            coalesce(shipping_province, ''),
-            cast(gateway_installments as varchar(10)),
-            coalesce(order_source, ''),
-            coalesce(social_network, ''),
-            coalesce(source_details, ''),
-            cast(orders_free_shipping as varchar(10)),
-            coalesce(source_type, '')
-        )))) as row_key,
-        *
-    from src
-),
-
-final as (
-    {% if is_incremental() %}
-        select
-            
-            s.store_id,
-            s.country,
-            s.domain,
-            s.vertical,
-            s.province,
-            s.city,
-            s.region,
-            s.business_size,
-            s.segment,
-            s.nice_9_name,
-            s.plan_group,
-            s.bu,
-
-            s.date,
-            s.year_month_day_code,
-            s.mes,
-            s.platform_type,
-            s.storefront,
-            s.device,
-            s.payment_provider,
-            s.payment_method,
-            s.shipping_method,
-            s.shipping_province,
-            s.gateway_installments,
-            s.order_source,
-            s.social_network,
-            s.source_details,
-            s.source_type,
-
-            s.gmv,
-            s.gmv_usd,
-            s.orders,
-            s.orders_free_shipping,
-            s.products,
-
-            -- auditoria
-            s.row_key,
-            coalesce(t.sys_audit_created_on, current_timestamp) as sys_audit_created_on,
-            coalesce(t.sys_audit_created_by, 'data-dev-dbt-products') as sys_audit_created_by,
-            current_timestamp as sys_audit_updated_on,
-            'data-dev-dbt-products' as sys_audit_updated_by
-        from src_with_key s
-        left join {{ this }} t
-          on s.row_key = t.row_key
-
-        {% set monitored_cols = ["orders", "gmv"] %}
-
-        where
-            t.row_key is null
-            {%- for col in monitored_cols %}
-              or (s.{{ col }} is distinct from t.{{ col }})
-            {%- endfor %}
-
-    {% else %}
-        -- Primeira carga: insere tudo
-        select
-            
-            s.store_id,
-            s.country,
-            s.domain,
-            s.vertical,
-            s.province,
-            s.city,
-            s.region,
-            s.business_size,
-            s.segment,
-            s.nice_9_name,
-            s.plan_group,
-            s.bu,
-
-            s.date,
-            s.year_month_day_code,
-            s.mes,
-            s.platform_type,
-            s.storefront,
-            s.device,
-            s.payment_provider,
-            s.payment_method,
-            s.shipping_method,
-            s.shipping_province,
-            s.gateway_installments,
-            s.order_source,
-            s.social_network,
-            s.source_details,
-            s.source_type,
-
-            s.gmv,
-            s.gmv_usd,
-            s.orders,
-            s.orders_free_shipping,
-            s.products,
-
-            -- auditoria
-            s.row_key,
-            current_timestamp as sys_audit_created_on,
-            'data-dev-dbt-products' as sys_audit_created_by,
-            current_timestamp as sys_audit_updated_on,
-            'data-dev-dbt-products' as sys_audit_updated_by
-        from src_with_key s
-    {% endif %}
+            cast(o.store_id as varchar(10)),
+            cast(o.country as varchar(10)),
+            coalesce(o.domain, ''),
+            coalesce(o.vertical, ''),
+            coalesce(o.province, ''),
+            coalesce(o.city, ''),
+            coalesce(o.region, ''),
+            coalesce(o.business_size, ''),
+            coalesce(o.segment, ''),
+            coalesce(o.nice_9_name, ''),
+            coalesce(o.plan_group, ''),
+            coalesce(o.bu, ''),
+            cast(o.date as varchar(10)),
+            cast(o.year_month_day_code as varchar(10)),
+            cast(o.mes as varchar(10)),
+            coalesce(o.platform_type, ''),
+            coalesce(o.storefront, ''),
+            coalesce(o.device, ''),
+            coalesce(o.payment_provider, ''),
+            coalesce(o.payment_method, ''),
+            coalesce(o.shipping_method, ''),
+            coalesce(o.shipping_province, ''),
+            cast(o.gateway_installments as varchar(10)),
+            coalesce(o.order_source, ''),
+            coalesce(o.social_network, ''),
+            coalesce(o.source_details, ''),
+            cast(o.orders_free_shipping as varchar(10)),
+            coalesce(o.source_type, '')
+        )))) AS row_key
+    FROM {{ ref('_int__operations__orders_gmv_store__agg_daily_prep') }} o
 )
 
-select * from final
+SELECT
+    b.*,
+    COALESCE(e.sys_audit_created_on, current_timestamp) AS sys_audit_created_on,
+    COALESCE(e.sys_audit_created_by, 'data-dev-dbt-products') AS sys_audit_created_by,
+    current_timestamp AS sys_audit_updated_on,
+    'data-dev-dbt-products' AS sys_audit_updated_by
+FROM base_data b
+LEFT JOIN existing_data e ON b.row_key = e.row_key
+WHERE
+    {% if is_incremental() %}
+        e.row_key IS NULL
+        OR b.orders IS DISTINCT FROM e.orders
+        OR b.gmv IS DISTINCT FROM e.gmv
+    {% else %}
+        TRUE
+    {% endif %}
