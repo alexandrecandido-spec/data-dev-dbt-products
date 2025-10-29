b422757547e70091b331d3ad7bbd670e59efad91
Felipe Lorenzo <felipe.lorenzo@tiendanube.com>
2025-10-29 10:52:51 -0300

    Readapting logic to company metrics paid orders

--- nubeproduct/models/intermediate/partnerships/_int_partnerships__agencies_stores_gmv_orders_daily_construction.sql
+++ nubeproduct/models/intermediate/partnerships/_int_partnerships__agencies_stores_gmv_orders_daily_construction.sql
@@ -1,15 +1,16 @@
 SELECT 
     PO.store_id,
-    PO.date AS order_date,
-    COALESCE(SUM(PO.gmv), 0) AS gmv_local_currency_daily,
-    COALESCE(SUM(PO.gmv_usd), 0) AS gmv_usd_daily,
-    COALESCE(SUM(PO.orders), 0) AS orders_daily,
+    DATE(PO.completed_at) AS order_date,
+    COALESCE(SUM(PO.total), 0) AS gmv_local_currency_daily,
+    COALESCE(SUM(PO.total_in_usd), 0) AS gmv_usd_daily,
+    COALESCE(COUNT(PO.id), 0) AS orders_daily,
     MAX(PO.sys_audit_updated_on) AS construction_change_timestamp
-FROM {{ ref('g__operations__orders_gmv_store__agg_daily') }} AS PO
+FROM {{ ref('company_metrics_paid_orders') }} AS PO
 INNER JOIN {{ ref('s__general__partners_stores__ref') }} AS PS
     ON PO.store_id = PS.store_id
-WHERE PO.platform_type = 'on'
+WHERE PO.completed_at IS NOT NULL
+    AND PO.platform_type = 'on'
     AND PS.partnership_type = 'store_development'
 GROUP BY 
     PO.store_id, 
-    PO.date
+    DATE(PO.completed_at)
