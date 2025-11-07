09a6b1d7ca16b982a6fb19a9d38b3c1b2e4633d5
jhumktsense <jhu@marketsense.tech>
2025-11-07 17:26:52 -0300

    fix(merchant): Correct tag to daily-8am-8pm for store_identity model

--- nubeproduct/models/data_product/merchant/s__attributes__store_identity__ref.sql
+++ nubeproduct/models/data_product/merchant/s__attributes__store_identity__ref.sql
@@ -3,7 +3,7 @@
         materialized='incremental',
         incremental_strategy='merge',
         unique_key='store_id',
-        tags=['daily-10am-10pm'],
+        tags=['daily-8am-8pm'],
         post_hook=[
             "DELETE FROM {{ this }} WHERE store_id IN (SELECT store_id FROM {{ ref('merchant__attributes__store_info__ref') }} WHERE state = 4)"
         ]
