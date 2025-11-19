cdce6bf968ead62b45b1fa60e9e12feb46c8b5ba
Sofia Torres <sofia.torres@tiendanube.com>
2025-11-19 09:15:18 -0300

    Remove 'marketing' tag from monthly GMV DP; keep only daily-10am/10pm scheduling

--- nubeproduct/models/data_product/marketing/g__general__deepdive_gmv_store__agg_monthly.sql
+++ nubeproduct/models/data_product/marketing/g__general__deepdive_gmv_store__agg_monthly.sql
@@ -5,7 +5,7 @@
     partition_by         = ['year_month_code'],
     cluster_by           = ['store_id'],
     on_schema_change     = 'fail',
-    tags                 = ["marketing","daily-10am","daily-10pm"]
+    tags                 = ["daily-10am","daily-10pm"]
 ) }}
 
 with
