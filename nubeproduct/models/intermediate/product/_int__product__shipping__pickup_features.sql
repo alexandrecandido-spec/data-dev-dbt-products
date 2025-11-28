8dc7aa121c753dbb5611dc3d339e5e4680164d46
Matias Scasso <matias.scasso@tiendanube.com>
2025-11-28 17:10:27 -0300

    changes to model

--- nubeproduct/models/intermediate/product/_int__product__shipping__pickup_features.sql
+++ nubeproduct/models/intermediate/product/_int__product__shipping__pickup_features.sql
@@ -1,11 +1,13 @@
 -- _int__product__shipping__pickup_features
 
+WITH ua as (
+
 SELECT
     pb.store_id,
     pb.pickup_feature,
     pb.is_deleted,
-    'non applicable' as max_km,
-    'non applicable' as max_days_pickup,
+    0 as max_km,
+    0 as max_days_pickup,
     pb.pickup_locations_count
 
 FROM {{ ref('_int__product__shipping__pickup_branch') }} pb
@@ -16,8 +18,8 @@ SELECT
     pc.store_id,
     pc.pickup_feature,
     pc.is_deleted,
-    'non applicable' as max_km,
-    'non applicable' as max_days_pickup,
+    0 as max_km,
+    0 as max_days_pickup,
     pc.pickup_locations_count
 
 FROM {{ ref('_int__product__shipping__pickup_custom') }} pc
@@ -34,4 +36,35 @@ SELECT
 
 FROM {{ ref('_int__product__shipping__pickup_point') }} pp
 
+),
 
+final as (
+
+SELECT
+    ss.store_id,
+    CASE WHEN ua.pickup_feature IS NULL THEN 'none' ELSE ua.pickup_feature END AS pickup_feature,
+    CASE WHEN ua.is_deleted IS NULL THEN FALSE ELSE ua.is_deleted END AS is_deleted,
+    CASE WHEN ua.max_km IS NULL THEN 0 ELSE ua.max_km END AS max_km,
+    CASE WHEN ua.max_days_pickup IS NULL THEN 0 ELSE ua.max_days_pickup END AS max_days_pickup,
+    CASE WHEN ua.pickup_locations_count IS NULL THEN 0 ELSE ua.pickup_locations_count END AS pickup_locations_count
+
+FROM {{ ref('s__lifecycle__store_status__ref') }} ss
+
+JOIN {{ ref('s__attributes__store_core__ref') }} sc
+    ON sc.store_id = ss.store_id
+
+LEFT JOIN ua
+    ON ua.store_id = ss.store_id
+)
+
+SELECT
+    store_id,
+    pickup_feature,
+    is_deleted,
+    max_km,
+    max_days_pickup,
+    SUM(pickup_locations_count) AS pickup_locations_count
+
+FROM final
+
+GROUP BY 1, 2, 3, 4, 5
