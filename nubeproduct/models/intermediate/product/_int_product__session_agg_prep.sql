1947aaf2d4a015d3c6a24afa89cc962bd43eda4b
Barbi <167924360+datansbam@users.noreply.github.com>
2025-12-02 11:13:36 -0300

    Implements replace_where incremental strategy for sessions gold models (#689)

--- nubeproduct/models/intermediate/product/_int_product__session_agg_prep.sql
+++ nubeproduct/models/intermediate/product/_int_product__session_agg_prep.sql
@@ -7,22 +7,24 @@ SELECT
     , base_sessions.session_id
     , base_sessions.consumer_id
     , base_sessions.store_id
-    , merch_core.country_code
-    , merch_core.vertical_name
-    , merch_stats.current_plan_type
-    , merch_stats.current_segment
-    , merch_stats.is_store_blocked
-    , base_sessions.visitor_country
-    , base_sessions.device
-    , base_sessions.theme
-    , base_sessions.source_name
-    , base_sessions.source_group
-    , base_sessions.google_subchannel
-    , base_sessions.traffic_type
-    , base_sessions.is_end_user
+    , COALESCE(merch_core.country_code, 'undefined') AS country_code
+    , COALESCE(merch_core.vertical_name, 'undefined') AS vertical_name
+    , COALESCE(merch_stats.current_plan_type, 'undefined') AS current_plan_type
+    , COALESCE(merch_stats.current_segment, 'undefined') AS current_segment
+    , COALESCE(merch_stats.is_store_blocked, FALSE) AS  is_store_blocked
+    , COALESCE(base_sessions.visitor_country, 'undefined') AS visitor_country
+    , COALESCE(base_sessions.device, 'undefined') AS device
+    , COALESCE(base_sessions.theme, 'undefined') AS theme
+    , COALESCE(base_sessions.source_name, 'others') AS source_name
+    , COALESCE(base_sessions.source_group, 'others') AS source_group
+    , COALESCE(base_sessions.google_subchannel, 'not_google') AS google_subchannel
+    , COALESCE(base_sessions.traffic_type, 'organic') AS  traffic_type
+    , COALESCE(base_sessions.is_end_user, TRUE) AS is_end_user
     , base_carts.cart_id
-    , mo.storefront
-    , mo.total_in_usd
+    , CASE  WHEN mo.storefront IS NOT NULL THEN mo.storefront
+            WHEN mo.storefront IS NULL AND mo.id IS NOT NULL THEN 'unknown'
+            ELSE 'undefined' END AS storefront
+    , mo.total / exchange_rate.direct_exchange_rate AS total_in_usd
 FROM
     {{ ref('s__traffic__session__event') }} AS base_sessions
 INNER JOIN --removes stores with state = 4
@@ -40,13 +42,17 @@ LEFT JOIN
     ON base_carts.cart_id = mo.id
     AND mo.is_paid_order = TRUE
     AND mo.total_in_usd >= 0 
-    AND mo.total_in_usd < 10000
+    AND mo.total_in_usd <= 10000
+LEFT JOIN 
+    {{ ref('finance_exchange_rate') }} AS exchange_rate
+    ON DATE(mo.completed_at) = DATE(exchange_rate.processed_at) 
+    AND mo.currency = exchange_rate.isocode
 WHERE
     {% set interval = get_max_date_env_model(
       'product',
       'g__traffic__session_carts__agg_daily',
       'base_date',
-      1, 'month',
+      1, 'MONTH',
       fallback_start='2024-01-01',
       fallback_end='2024-01-05'
     ) %}
